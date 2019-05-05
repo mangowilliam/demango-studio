e11afe4e1d9901d55eb0f6fb051d82527bf9cd65
mangowilliam <williammango2015@gmail.com>
2019-05-05 21:28:36 +0300

    add toggle jquery to all icons

--- js/demango.js
+++ js/demango.js
@@ -15,17 +15,25 @@ $(document).ready(function() {
   $("#dsn").click(function() {
     $(".btn_dsn").toggle();
     $("#dsn").hide();
-
   });
-
 });
 $(document).ready(function() {
   $(".btn_dev").click(function() {
     $("#dev").toggle();
+    $(".btn_dev").hide();
+  });
+  $("#dev").click(function() {
+    $(".btn_dev").toggle();
+    $("#dev").hide();
   });
 });
 $(document).ready(function() {
   $(".btn_mgt").click(function() {
     $("#mgt").toggle();
+    $(".btn_mgt").hide();
+  });
+  $("#mgt").click(function() {
+    $(".btn_mgt").toggle();
+    $("#mgt").hide();
   });
 });
