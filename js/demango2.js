7fa3518ceee61b16fc6f52e5e0429a3578c43644
mangowilliam <williammango2015@gmail.com>
2019-05-08 09:38:31 +0300

    update project name

---
+++ js/demango2.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+      $("#work1").hover(function() {
+        $(this).css("background-color", "#FFFFFF");
+      }, function() {
+        $(this).css("background-color", "white");
+      });
+      $("#work2").hover(function() {
+        $(this).css("background-color", "#FFB6C1");
+      }, function() {
+        $(this).css("background-color", "white");
+      });
+      $("#work3").hover(function() {
+        $(this).css("background-color", "#FFB6C1");
+      }, function() {
+        $(this).css("background-color", "white");
+      });
+      $("#work4").hover(function() {
+        $(this).css("background-color", "#FFB6C1");
+      }, function() {
+        $(this).css("background-color", "white");
+      });
+      $("#work5").hover(function() {
+        $(this).css("background-color", "#FFB6C1");
+      }, function() {
+        $(this).css("background-color", "white");
+      });
+      $("#work6").hover(function() {
+        $(this).css("background-color", "#FFB6C1");
+      }, function() {
+        $(this).css("background-color", "white");
+      });
+      $("#work7").hover(function() {
+        $(this).css("background-color", "#FFB6C1");
+      }, function() {
+        $(this).css("background-color", "white");
+      });
+      $("#work8").hover(function() {
+        $(this).css("background-color", "#FFB6C1");
+      }, function() {
+        $(this).css("background-color", "white");
+      });
+      $("#result").click(function() {
+            if (user == "") {
+              alert("enter your name");
+            });
